b51ac7c78606d314d1d189d8ca14b95f94c31155
JUAN-CAMILOC279 <>
2022-05-06 16:23:29 +0000

    commit1

--- Tema4/main.cpp
+++ Tema4/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     cin >> b;
     int S = a + b;
     int P = a *b;
-	cout << "a+b=" << S << "\n";
+	cout << "a+b=" <<             S << "\n";
     cout << "a*b=" << P << "\n";
 	return 0;
 }
